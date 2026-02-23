6b983dd30981e65f2df9adada2742bedbb6cb3cb
fix777 <fix777.jerry@outlook.com>
2026-02-22 19:30:37 -0500

    v6.8.26-0-0-plus

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 set -u
 
 CGO_ENABLED=0 go build -ldflags "-s -w \
-  -X 'main.Version=6.7.45-1-plus' \
+  -X 'main.Version=v6.8.26-0-0-plus' \
   -X 'main.Commit=$(git rev-parse --short HEAD)' \
   -X 'main.BuildDate=$(date -u +%Y-%m-%dT%H:%M:%SZ)'" \
   -o cli-proxy-api-plus ./cmd/server/
